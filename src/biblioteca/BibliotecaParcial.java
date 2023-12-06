b3a8392b83e2603f1776bf3b66e6cc524ad133c3
faber navia <97317782+FaberGG@users.noreply.github.com>
2023-12-06 10:45:00 -0500

    Update BibliotecaParcial.java

--- src/biblioteca/BibliotecaParcial.java
+++ src/biblioteca/BibliotecaParcial.java
@@ -11,6 +11,10 @@ package biblioteca;
 public class BibliotecaParcial {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Autor autor1 = new Autor("Teresa", "Reino Unido", "2021");
+        Libro libro1 = new Libro("Harry Potter XVI", "novela", "Norma", "2024");
+        Libro libro2 = new Libro("Think about you", "poema", "MHRD", "2001");
+// En proceso :)
+        
     }
 }
